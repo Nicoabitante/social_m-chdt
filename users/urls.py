5f45adbc2e5a2d56d9b4968dbe630c82267bffe5
Nico Ab <nicolas.abitante@agrology.com>
2024-07-03 17:54:35 -0300

    added follow

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import UserList, UserDetail
+from users.views import UserList, UserDetail, UserMatch
 
 urlpatterns = [
     path('', UserList.as_view(), name='Users'),
-    path('<int:pk>/', UserDetail.as_view(), name='User')
+    path('<int:pk>/', UserDetail.as_view(), name='User'),
+    path('<int:follower_id>/follow/<int:user_id>', UserMatch.as_view(), name='Follow a user')
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from users.models import User
 from users.serializers import UserSerializer, UserDetailSerializer
@@ -12,3 +15,15 @@ class UserList(generics.ListCreateAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserDetailSerializer
+
+
+class UserMatch(APIView):
+    def post(self, request, follower_id, user_id):
+        follower = generics.get_object_or_404(User, id=follower_id)
+        user = generics.get_object_or_404(User, id=user_id)
+
+        if user == follower:
+            return Response({'error': 'You cannot follow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        follower.following.add(user)
+        return Response({'status': 'Following user successfully.'}, status=status.HTTP_200_OK)
